b835da3db6b6425067caae067fa5bdfa53c424ac
Noam Dev <noamdev@h-MacBook-Pro-sl-nm.local>
2022-07-26 18:25:52 +0300

    Fix: page limit and skip error

--- server/app/services/query.js
+++ server/app/services/query.js
@@ -1,10 +1,10 @@
-const DEFAULT_PAGE_LIMIT = 0
+const DEFAULT_PAGE_LIMIT = 10
 const DEFAULT_PAGE_NUMBER = 1
 
 function getPagination(query) {
-    const limit = Math.abs(query.limit) || DEFAULT_PAGE_NUMBER
-    const page = Math.abs(query.page) || DEFAULT_PAGE_LIMIT
-    const skip = (page - 1) * limit
+    const limit = Math.abs(query.limit) || DEFAULT_PAGE_LIMIT
+    const page = Math.abs(query.page) || DEFAULT_PAGE_NUMBER
+    const skip = (page-1) * limit
 
     return {
         skip,
